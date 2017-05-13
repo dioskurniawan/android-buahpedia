8ae5ae42cb78276406c361c6c75f5204e501cbec
omrobbie <putu.roby@yahoo.com>
2017-05-14 00:05:14 +0800

    tampilkan list data

--- app/src/main/java/com/omrobbie/buahpedia/MainActivity.java
+++ app/src/main/java/com/omrobbie/buahpedia/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // set layout ke listview, jika gambar cover di klik
         ImageView imgCover = (ImageView) findViewById(R.id.imgCover);
         imgCover.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
                 setContentView(R.layout.listview);
             }
         });
+
+        final ListView lvList = (ListView) findViewById(R.id.lvList);
+
+        // masukkan item ke dalam data
+        final ArrayList<ListData> data = new ArrayList<>();
+        data.add(new ListData(R.drawable.icon, "Test", "Test 1"));
+
+        // set adapter
+        final ListAdapter adapter = new ListAdapter(this, R.layout.listitem, data);
+        // lvList.setAdapter(adapter);
     }
 }
