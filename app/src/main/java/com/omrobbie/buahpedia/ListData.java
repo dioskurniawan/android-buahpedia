5737a333b3c72d4693043b5d3390006c62dd0134
omrobbie <putu.roby@yahoo.com>
2017-05-13 23:01:49 +0800

    bikin ListData untuk menampung data item

---
+++ app/src/main/java/com/omrobbie/buahpedia/ListData.java
@@ -0,0 +1,13 @@
+package com.omrobbie.buahpedia;
+
+public class ListData {
+    public int image;
+    public String title;
+    public String description;
+
+    public ListData(int image, String title, String description) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+    }
+}
