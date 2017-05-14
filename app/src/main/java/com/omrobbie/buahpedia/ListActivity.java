a189818d41f3c9175d4698ef53fa9c392c14f2e4
omrobbie <putu.roby@yahoo.com>
2017-05-14 11:33:17 +0800

    tambahkan gambar-gambar buah

--- app/src/main/java/com/omrobbie/buahpedia/ListActivity.java
+++ app/src/main/java/com/omrobbie/buahpedia/ListActivity.java
@@ -17,7 +17,15 @@ public class ListActivity extends Activity {
 
         // masukkan item ke dalam data
         ArrayList<ListData> data = new ArrayList<>();
-        data.add(new ListData(R.drawable.icon, "Title", "Short description"));
+        data.add(new ListData(R.drawable.buah_anggur, "Anggur", "Keterangan tentang buah anggur."));
+        data.add(new ListData(R.drawable.buah_apel, "Apel", "Keterangan tentang buah apel."));
+        data.add(new ListData(R.drawable.buah_jeruk, "Jeruk", "Keterangan tentang buah jeruk."));
+        data.add(new ListData(R.drawable.buah_kiwi, "Kiwi", "Keterangan tentang buah kiwi."));
+        data.add(new ListData(R.drawable.buah_mangga, "Mangga", "Keterangan tentang buah mangga."));
+        data.add(new ListData(R.drawable.buah_manggis, "Manggis", "Keterangan tentang buah manggis."));
+        data.add(new ListData(R.drawable.buah_nanas, "Nanas", "Keterangan tentang buah nanas."));
+        data.add(new ListData(R.drawable.buah_pepaya, "Pepaya", "Keterangan tentang buah pepaya."));
+        data.add(new ListData(R.drawable.buah_semangka, "Semangka", "Keterangan tentang buah semangka."));
 
         // set adapter
         ListAdapter adapter = new ListAdapter(this, R.layout.listitem, data);
